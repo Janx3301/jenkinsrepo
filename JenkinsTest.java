750da19740b6b0a6b6ada0cd3fa55dce5dab4cdf
Janwin Joy <janwinjoy@protonmail.com>
2024-06-27 15:24:03 +0530

    small update

--- JenkinsTest.java
+++ JenkinsTest.java
@@ -1,6 +1,8 @@
 public class JenkinsTest {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
+          System.out.println("Small update");
+            System.out.println("Blah Blah");
         
     }
 }
